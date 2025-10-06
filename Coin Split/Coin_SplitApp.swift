2211b146388289261a8273cf318ec8877c983f94
Tens1des <romankotov3213@gmail.com>
2025-10-06 18:52:32 +0300

    Initial Commit

---
+++ Coin Split/Coin_SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coin_SplitApp.swift
+//  Coin Split
+//
+//  Created by Рома Котов on 06.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Coin_SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coin Split/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Coin Split
+//
+//  Created by Рома Котов on 06.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
